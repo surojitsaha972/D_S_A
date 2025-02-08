2d450f8234c7e38916cd9bce565e472d415ad894
surojitsaha972 <sahasurojit972@gmail.com>
2025-02-08 23:53:08 +0530

    modified

---
+++ DSA Python/stack using modules.py
@@ -0,0 +1,12 @@
+# Collections =>> deque(double ended queue)
+
+# import collections
+# stack=collections.deque()
+# print(stack)
+# stack.append(10)
+# stack.append(20)
+# stack.append(30)
+# print(stack)
+# stack.pop()
+# print(stack)
+
