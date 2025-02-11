c3531e1268437895eae4ebe2127a9f683a44287c
surojitsaha972 <sahasurojit972@gmail.com>
2025-02-11 23:56:09 +0530

    Updated

---
+++ DSA Python/Queue using classes.py
@@ -0,0 +1,20 @@
+# -----------------deque ==> Collections --------------------
+
+# import collections
+# q=collections.deque()
+# print(q)
+# q.appendleft(10)
+# q.appendleft(20)
+# print(q)
+# q.pop()
+# q.pop()
+# print(q)
+
+# -------------------queue ==> Queue----------------------------
+
+import queue
+q=queue.Queue()
+q.put(10)
+print(q)
+q.get()
+print(q)
