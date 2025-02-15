d7a0479b8cddf9c4aa1c1043ebcb51a0f75f3210
surojitsaha972 <sahasurojit972@gmail.com>
2025-02-15 22:04:26 +0530

    Linked list

---
+++ DSA Python/Singly Linked List.py
@@ -0,0 +1,11 @@
+# .............LINKED LIST............
+
+# It is a linear data structures. It is a collection of nodes that are linked with each other. 
+# A node contains two things first is data and second is a link that connects it with another node.
+# Node: elements of linked lists.
+# Advantages: Dynamic DS, Insertion & Deletion is easy, can implement stack queue & graph.
+# Disadvantages: Needs extra memeory, Random access not possible
+# Type: Singly Linked List, Doubly Linked List, Circular Linked List.
+
+# ...............SINGLY LINKED LIST.............
+
